601988e5535605de1e8b5bfdb3e8be8bb8a62bf7
tgcl21 <tgcl21@inf.ufpr.br>
2024-04-02 15:25:17 -0300

    initial commit

---
+++ perfSL.c
@@ -0,0 +1,36 @@
+#include "perfSL.h"
+
+int_t encontraMax(real_t **A, int_t i, int_t n){
+    real_t max = 0;
+    int_t iPivo = i;
+
+    for (int j = 0; j < n; i++) {
+        if (ABS(A[i][j]) > max) {
+            max = ABS(A[i][j]);
+            iPivo = i;
+        }
+    }
+
+}
+
+void eliminacaoGaussPivoteamento(real_t **A, real_t *b, int_t n){
+    
+    for (int i=0; i < n; i++) {
+        int_t iPivo = encontraMax(A, i, n);
+        
+        if(i != iPivo)
+            trocaLinha(A, b, i, iPivo, n);
+
+        for (int k = i+1; k < n; k++){
+            real_t m = A[k][i] / A[i][i];
+            A[k][i] = 0;
+
+            for (int j = i+1; j < n; j++){
+                A[k][j] -= m * A[i][j];
+            }
+            
+            b[k] -= m * b[i];
+        }    
+    }
+
+}
---
+++ perfSL.h
@@ -0,0 +1,14 @@
+#ifndef PERFSL_H
+#define PERFSL_H
+
+#include "utils.h"
+#include <stdlib.h>
+#include <math.h>
+
+int_t encontraMax(real_t **A, int_t i, int_t n){
+
+void eliminacaoGaussPivoteamento(real_t **A, real_t *b, int_t n);
+
+void leEntrada(real_t **A, real_t *b, int_t n);
+
+#endif
