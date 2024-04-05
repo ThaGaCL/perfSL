3d3b8a566c3528d4cd721b1a6fdd2af502616ad4
ThaGaCL <tgcl21@inf.ufpr.br>
2024-04-05 16:35:15 -0300

    err: gs-tri not working

--- main.c
+++ main.c
@@ -21,49 +21,73 @@ int main(int argc, char *argv[])
 
     leEntrada(A, b, n);
 
-    // imprimeEntrada(A, b, n);
+    // imprimeEntrada(A, b, n);s
 
-    // copiaEntrada(A, b, Ac, bc, n);
+    copiaEntrada(A, b, Ac, bc, n);
 
     // EG - Pivoteamento
-    // eliminacaoGaussPivoteamento(Ac, bc, n);
+    eliminacaoGaussPivoteamento(Ac, bc, n);
     // // imprimeEntrada(Ac, bc, n);
 
     // // Vetor solucao
     real_t *x = malloc(n * sizeof(real_t));
     real_t *r = malloc(n * sizeof(real_t));
-    // retrosSubs(Ac, bc, x, n);
-    // calculaResiduo(A, x, b, r, n);
-    // imprimeSaida(x, n, r);
+    retrosSubs(Ac, bc, x, n);
+    calculaResiduo(A, x, b, r, n);
+    imprimeSaida(x, n, r);
 
-    // copiaEntrada(A, b, Ac, bc, n);
-    // copiaEntrada(A, b, Ac, bc, n);
+    copiaEntrada(A, b, Ac, bc, n);
+    copiaEntrada(A, b, Ac, bc, n);
 
     // GS - Iterativo
-    // real_t *x1 = malloc(n * sizeof(real_t));
-    // iterativoGaussSeidel(A, b, x1, n, 0.0001, 50);
-    // calculaResiduo(A, x1, b, r, n);
-    // imprimeSaida(x1, n, r);
-    // imprimeEntrada(A, x1, n);
+    real_t *x1 = malloc(n * sizeof(real_t));
+    real_t *r1 = malloc(n * sizeof(real_t));
+    iterativoGaussSeidel(Ac, bc, x1, n, 0.0001, 50);
+    calculaResiduo(Ac, x1, bc, r1, n);
+    imprimeSaida(x1, n, r1);
 
     // EG - TRIDIAGONAL
     // Vetores diagonais
+    
+    copiaEntrada(A, b, Ac, bc, n);
+    
     real_t *c = malloc((n - 1) * sizeof(real_t));
     real_t *d = malloc(n * sizeof(real_t));
     real_t *a = malloc((n - 1) * sizeof(real_t));
+    real_t *r2 = malloc(n * sizeof(real_t));
+    real_t *x2 = malloc(n * sizeof(real_t));
 
-    separaDiagonal(A, d, a, c, n);
+    separaDiagonal(Ac, d, a, c, n);
 
-    eliminacaoGaussTridiagonal(d, a, c, b, x, n);
+    eliminacaoGaussTridiagonal(d, a, c, bc, x2, n);
+    calculaResiduo(Ac, x2, bc, r2, n);
+    imprimeSaida(x2, n, r2);
+
+    // GS - TRIDIAGONAL
+    real_t *r3 = malloc(n * sizeof(real_t));
+    real_t *x3 = malloc(n * sizeof(real_t));
     
-    // imprimeEntrada(A, b, n);
-    // calculaResiduo(A, x, b, r, n);
-    imprimeSaida(x, n, r);
+    copiaEntrada(A, b, Ac, bc, n);
+
+    gaussSeidelTridiagonal(a, d, c, x3, bc, n, 0.0001, 50);
+    calculaResiduo(Ac, x3, bc, r3, n);
+    imprimeSaida(x3, n, r3);
+
     // Libera memoria alocada
     liberaMemoria(A, b, n);
     liberaMemoria(Ac, bc, n);
     free(x);
-    // free(x1);
-
+    free(x1);
+    free(x2);
+    free(x3);
+    free(r);
+    free(r1);
+    free(r2);
+    free(r3);
+    free(a);
+    free(c);
+    free(d);
+    
+    
     return 0;
 }
--- perfSL.c
+++ perfSL.c
@@ -152,6 +152,22 @@ void eliminacaoGaussTridiagonal(real_t *d, real_t *a, real_t *c, real_t *b, real
         x[i] = (b[i] - c[i] * x[i + 1]) / d[i];
 }
 
+void gaussSeidelTridiagonal(real_t *a, real_t *b, real_t *c, real_t *x, real_t *d, int_t n, real_t tol, int_t maxIter)
+{
+    printf("GS tridiagonal [ %ld iterações ]:\n", maxIter);
+    real_t erro = 1.0 + tol;
+    while (erro < tol && maxIter > 0)
+    {
+        x[0] = (b[0] - c[0] * x[1]) / d[0];
+        for (int i = 1; i < n - 1; ++i)
+
+            x[i] = (b[i] - a[i - 1] * x[i - 1] - c[i] * x[i + 1]) / d[i];
+
+        x[n - 1] = (b[n - 1] - a[n - 2] * x[n - 2]) / d[n - 1];
+        maxIter--;
+    }
+}
+
 real_t calculaResiduo(real_t **A, real_t *x, real_t *b, real_t *residuo, int_t n)
 {
     int_t i, j;
